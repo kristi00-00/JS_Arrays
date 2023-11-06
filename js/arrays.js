fc0f0674920fb21f68c4ef883a3c64e958f5b193
“kristi00-00” <kristi_kristi00@mail.ru>
2023-11-07 01:44:33 +0300

    изменения

--- js/arrays.js
+++ js/arrays.js
@@ -7,6 +7,7 @@ arr=[4,16,36,25];
 let sum=arr.filter(index=>index%2==0).reduce((sum,elem)=>sum+Math.sqrt(elem),0);
 alert(sum);
 }
+
 function task2()
 {
     let arr = Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
