914b2474e98e1d283cefd54e2a6647cd31164feb
“kristi00-00” <kristi_kristi00@mail.ru>
2023-11-09 16:52:56 +0300

    задача 5 решена

--- js/arrays.js
+++ js/arrays.js
@@ -16,10 +16,12 @@ let newArr=arr.filter(elem=>elem>0 && elem<10);
 newArr=newArr.reduce((sum,elem)=>sum+elem,0);
 console.log(newArr);
 }
+
 function isChetn(arr)
 {
   return arr.every((elem)=>elem%2==0);
 }
+
 function calcFive(arr) 
 {
  let newArr=arr.filter((elem)=>elem%5==0);
@@ -28,4 +30,16 @@ function calcFive(arr)
 function task4(){
 let arr=[1,2,5,12,15,21];
 console.log(calcFive(arr));
+} 
+
+function average(arr){
+  let summa = arr.reduce((sum,elem)=>sum+elem,0);
+  let elem = arr.length;
+  return summa/elem;
 }
+function task5(){
+  let arr=Array.from(Array(10),()=>{return (Math.random()*20)});
+  console.log(arr.toString()); 
+  let x=average(arr);
+   console.log(x.toFixed(1));
+  }
