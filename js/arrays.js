f125c2f8aee33c1220d0fe9c08c64dd7c60470ad
“kristi00-00” <kristi_kristi00@mail.ru>
2023-11-12 15:31:40 +0300

    Задача 7 решена

--- js/arrays.js
+++ js/arrays.js
@@ -83,3 +83,19 @@ function task9()
   let res=arr.filter(newArr=>newArr.startsWith('http://'));
   console.log(res);
 }
+function task7(){
+  let arr = Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
+ console.log(arr.toString());
+ let summaChetn=arr.reduce((sum,elem,index)=>{
+  if (index%2==0) sum=sum+elem;
+  return sum;
+},0);
+let summaNechetn=arr.reduce((sum,elem,index)=>{
+if (index%2!=0) sum=sum+elem;
+return sum;
+},0);
+console.log(summaChetn);
+console.log(summaNechetn);
+console.log(summaChetn/summaNechetn);
+
+}
